b9ffca7b6b0c16aefc55f6d7357415de442a9cfc
Larissa <larissa.pereiraf12@gmail.com>
2026-09-14 22:54:20 -0300

    02_operações_matemáticas.py

---
+++ 02_Operações_Matematicas.py
@@ -0,0 +1,24 @@
+# Calcular: 2 mais 3 vezes 3 
+print(2 + (3*3))
+
+
+# Calcular 4 elevado a 2, dividido por 3
+print ((4**2)/3)
+
+
+# Calcular: 9 elevado a 2 mais 2 vezes 6 menos 1
+print ((((9**2)+2)*6)-1)
+
+
+# Calcular 7 elevado a 2 mais 3 vezes 4 menos 2
+print ((((7**2)+3)*4)-2)
+
+
+# Calcular: 5 elevado a 2 menos 1 vezes 3 mais 6
+print ((((5**2)-1)*3)+6)
+
+
+# Calcular 8 elevado a 2 mais 4 divido por 2 menos 5
+print ((((8**2)+4)/2)-5)
+
+
--- calcular.py
+++
@@ -1,2 +0,0 @@
-# Calcular: 2 mais 3 vezes 3 
-print(2 + (3*3))
