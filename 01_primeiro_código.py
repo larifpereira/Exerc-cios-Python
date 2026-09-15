ed87467500adaf4504e9671e804b0968844ecf50
Larissa <larissa.pereiraf12@gmail.com>
2026-09-14 22:56:12 -0300

    01_primeiro_código.py

--- 01_primeiro_código.py
+++ 01_primeiro_código.py
@@ -1,3 +1,7 @@
-# Meu primeiro código.
+# Meu "Olá Mundo!"
+print("Olá Mundo!)
+
+      
+# Meu primeiro código
 nome = input("Digite seu nome: ")
 print(f"Olá, {nome}! Seja bem-vindo ao mundo da programação.")
