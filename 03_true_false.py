0cbcb80af0fcb01d524d1379e336319fd63dbdb8
Larissa <larissa.pereiraf12@gmail.com>
2026-09-14 23:47:05 -0300

    03_true_false.py

---
+++ 03_true_false.py
@@ -0,0 +1,13 @@
+# Solicite e compare se dois números inteiros são iguais
+print("Digite dois números e veja se são iguais")
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+res = (a == b)
+print("{} e {} são: {} ".format(a,b,res))
+
+# Solicite e compare se dois números inteiros são diferentes
+print("Digite dois números e veja se são iguais")
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+res = (a != b)
+print("{} e {} são: {} ".format(a,b,res))
